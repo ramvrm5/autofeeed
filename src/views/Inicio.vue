0d3a2e179a2f7e8999a812918e97a1b66a05f79c
ram verma <ramvrm5@gmail.com>
2020-09-26 14:49:09 +0530

    bug fixed

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -60,141 +60,8 @@
               />
             </svg>
           </b-button>
-
-          <!-- Comments Modal Start  -->
-          <b-modal
-            :id="'modal-comment-'+index"
-            ref="modal"
-            title="Comments"
-            modal-class="comments-modal"
-            body-class="body-class-modal"
-            @hidden="resetModalll"
-          >
-            <b-container fluid style="padding: 0px;">
-              <b-row class="mb-1">
-                <b-col
-                  cols="8"
-                  class="comments-scrollBar"
-                  style="max-height: 440px;overflow-y:scroll;padding: 0px;"
-                >
-                  <b-card
-                    :key="index"
-                    :id="index"
-                    :title="item.titulo"
-                    :img-src="item.img"
-                    img-alt="Image"
-                    img-top
-                    tag="article"
-                  >
-                    <b-card-text>{{item.cuerpo}}</b-card-text>
-                    <template v-slot:footer>
-                      <div v-if="item.fecha">
-                        <small class="text-muted">Fecha: {{(item.fechaClasica).split("T")[0]}}</small>
-                      </div>
-                      <div v-if="item.tags">
-                        <small
-                          class="text-muted"
-                        >Intereses: {{item.tags.join(", ").replace('que,','').replace(' ,','')}}</small>
-                      </div>
-                      <div v-if="!item.tags">
-                        <small class="text-muted">Intereses: indefinidos</small>
-                      </div>
-                      <div v-if="item.fuente">
-                        <small class="text-muted">Fuente: {{(item.fuente).split("T")[0]}}</small>
-                      </div>
-                    </template>
-                  </b-card>
-                  <!-- <b-form-select v-model="headerBgVariant" :options="variants"></b-form-select> -->
-                </b-col>
-                <b-col cols="4" style="padding: 0px;">
-                  <b-row class="ml-0 mr-1" style="border-bottom: 2px solid rgb(223 223 223)">
-                    <b-col cols="4 pr-0">
-                      <img
-                        id="profilepic"
-                        class="rounded-circle"
-                        :src="imageProfile"
-                        onerror="this.onerror=null;this.src='../assets/avatar-01.png';"
-                        style="width: 58%;margin-bottom: 8px;margin-top: 8px;border: 2px solid #c5c1c140;"
-                      />
-                    </b-col>
-                    <b-col cols="8 pl-0 mt-3">
-                      <b>{{username}}</b>
-                    </b-col>
-                  </b-row>
-                  <b-row
-                    v-if="item.comentarios && item.comentarios.length > 0"
-                    style="max-height: 298px;overflow-y: scroll;"
-                    class="comments-scrollBar ml-0 mr-1"
-                    id="commentSection"
-                  >
-                    <b-row
-                      v-for="(comments, i) in item.comentarios"
-                      :key="i"
-                      :id="i"
-                      class="mt-2 mr-0 ml-0"
-                      style="border-bottom: 1px solid #dfdfdf;"
-                    >
-                      <b-col cols="3">
-                        <img
-                          id="avtarPic"
-                          class="rounded-circle"
-                          :src="comments.image?comments.image:avtarPic"
-                          onerror="this.onerror=null;this.src='../assets/avatar-01.png';"
-                          style="width: 90%;margin-bottom: 8px;margin-top: 8px;border: 2px solid #c5c1c140;"
-                        />
-                      </b-col>
-                      <b-col cols="9 pl-0">
-                        <b-row>
-                          <b-col
-                            cols="4 pl-3"
-                            style="padding-left: 27px;text-transform:capitalize;font-weight: 700;"
-                          >{{comments.name}}</b-col>
-                          <b-col
-                            cols="8 pl-0 pr-0"
-                            style="font-size: 13px;color: #969191;"
-                          >{{comments.time}} {{comments.date}}</b-col>
-                          <b-col cols="12" style="font-weight: 500;">{{comments.comment}}</b-col>
-                        </b-row>
-                      </b-col>
-                    </b-row>
-                  </b-row>
-                  <b-row
-                    v-if="!item.comentarios || item.comentarios.length == 0"
-                    style="max-height: 298px;overflow-y: scroll;color: #afaeae;"
-                    class="comments-scrollBar ml-0 mr-1 mt-5 text-center"
-                    id="commentSection"
-                  >
-                    <b-col cols="12">No comments yet</b-col>
-                  </b-row>
-                  <b-row style="position: absolute;bottom: 0px;width: 308px;">
-                    <b-col cols="12">
-                      <b-form-textarea
-                        id="addComment"
-                        v-model="addComent"
-                        class="form-control"
-                        placeholder="Añadir comentarios"
-                      ></b-form-textarea>
-                      <b-button
-                        @click="postCommets(item,index,noticias)"
-                        :disabled="!addComent"
-                        class="post-Button"
-                        variant="primary"
-                      >Post</b-button>
-                    </b-col>
-                  </b-row>
-                </b-col>
-              </b-row>
-            </b-container>
-
-            <template v-slot:modal-footer>
-              <div class="w-100"></div>
-            </template>
-          </b-modal>
-          <!-- Comments Modal Start -->
-
           <b-button
             :id="'comment-'+index"
-            v-b-modal="'modal-comment-' + index"
             style="float:right;margin-right:3%"
             target="_blank"
             variant="primary"
@@ -306,20 +173,8 @@ export default {
       classA: "likepulsado",
       classB: "likepulsado",
       translate_lan: "es",
-      addComent: null,
-      submittedNews: [],
-      language: "es",
       imageURL: null,
       username: "",
-      form: {
-        title: null,
-        description: null,
-        /* source: null,
-        sourceId: null, */
-        tags: [],
-        img: null,
-        url: null,
-      },
     };
   },
   created() {
@@ -334,22 +189,6 @@ export default {
       "translateText",
       "saveCreatedNews",
     ]),
-    setProfileImage() {
-      let user = firebase.auth().currentUser;
-      let fecha2 = new Date();
-      this.username = user.email;
-      var imgurl2 =
-        "https://firebasestorage.googleapis.com/v0/b/autofeed2020.appspot.com/o/avatares%2F" +
-        encodeURIComponent(user.email) +
-        ".jpg?alt=media&time=" +
-        fecha2.getTime();
-      this.imageProfile = imgurl2;
-      //this.avtarPic = imgurl2;
-      setTimeout(() => {
-        let scrollToBottom = document.getElementById("commentSection");
-        scrollToBottom.scrollTop = scrollToBottom.scrollHeight;
-      }, 100);
-    },
     darlike(href, idelemento) {
       let contador_veces = 0;
 
@@ -440,54 +279,6 @@ export default {
           });
         });
     },
-    async postCommets(object, index, comentArray) {
-      let doc_id = null;
-      let user_name = this.username.split("@")[0];
-      let currentDate = moment(new Date()).format("L");
-      let currenttime = moment(new Date()).format("HH:mm");
-      if (object.comentarios) {
-        object.comentarios.push({
-          name: user_name,
-          comment: this.addComent,
-          date: currentDate,
-          time: currenttime,
-          image: this.imageProfile,
-        });
-      } else {
-        object["comentarios"] = [
-          {
-            name: user_name,
-            comment: this.addComent,
-            date: currentDate,
-            time: currenttime,
-            image: this.imageProfile,
-          },
-        ];
-      }
-      this.addComent = null;
-      let scrollToBottom = document.getElementById("commentSection");
-      scrollToBottom.scrollTop = scrollToBottom.scrollHeight;
-      const noticiasRef = db.collection("noticias");
-      const snapshot = await noticiasRef.where("id", "==", object.id).get();
-      if (snapshot.empty) {
-        console.log("No matching documents.");
-        return;
-      }
-      snapshot.forEach((doc) => {
-        //console.log(doc.id, '=>', doc.data());
-        db.collection("noticias")
-          .doc(doc.id)
-          .update({
-            comentarios: object.comentarios,
-          })
-          .then(() => {
-            console.log("updated");
-          });
-      });
-    },
-    resetModalll() {
-      this.addComent = null;
-    },
     checkTranslateButton(item){
       if(item.idioma == 'en'){
         return true
@@ -528,75 +319,10 @@ export default {
       item.titulo = translatedTitleText[0];
       item.cuerpo = translatedTitleText[1];
     },
-    hideComentsmodal(id) {
-      this.$bvModal.hide(id);
-    },
   },
   computed: {
     ...mapState(["tareas", "noticias"]),
     ...mapState(["usuario", "keywordactual"]),
   },
-  // components: {
-  //   HelloWorld
-  // }
 };
 </script>
-<style>
-.comments-modal .modal-dialog.modal-md {
-  max-width: 840px !important;
-  margin: 2% 0px 0px 19%;
-}
-.comments-modal .modal-dialog.modal-md .modal-content .modal-body {
-  padding-top: 0px !important;
-  padding-bottom: 0px !important;
-}
-/* width */
-.comments-scrollBar::-webkit-scrollbar {
-  width: 6px;
-}
-
-/* Track */
-.comments-scrollBar::-webkit-scrollbar-track {
-  background: #f1f1f1;
-}
-
-/* Handle */
-.comments-scrollBar::-webkit-scrollbar-thumb {
-  background: #888;
-}
-
-/* Handle on hover */
-.comments-scrollBar::-webkit-scrollbar-thumb:hover {
-  background: #555;
-}
-.post-Button {
-  /*   style="position: absolute;top: 18%;right: 20px;background-color: snow;color: #0075dc;font-weight: 700;box-shadow: none;border: none;" */
-  position: absolute;
-  top: 18%;
-  right: 20px;
-  background-color: #ffffff00;
-  color: #0075dc;
-  font-weight: 700;
-  box-shadow: none;
-  border: none;
-}
-.post-Button:hover {
-  color: #64aaf5;
-  background-color: unset;
-  border-color: unset;
-}
-.post-Button.disabled,
-.post-Button:disabled {
-  color: #64aaf5;
-  background-color: unset;
-  border-color: unset;
-}
-.post-Button:focus {
-  box-shadow: unset;
-}
-.post-Button:not(:disabled):not(.disabled):active {
-  color: #0075dc;
-  background-color: unset;
-  border-color: unset;
-}
-</style>
